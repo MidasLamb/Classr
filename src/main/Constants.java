5f06d05fe458e2ecbb152a50b5377cf7f01271c4
Stijn Caerts <stijncaerts@gmail.com>
2017-02-27 11:11:29 +0100

    extraction of magic numbers in separate class

---
+++ src/main/Constants.java
@@ -0,0 +1,13 @@
+package main;
+
+public final class Constants {
+	
+	public static final int CONTAINER_WIDTH = 600;
+	public static final int CONTAINER_HEIGHT = 800;
+	
+
+	
+	private Constants() {
+		throw new AssertionError();
+	}
+}
