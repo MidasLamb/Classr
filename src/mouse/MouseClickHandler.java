e99f28c42dfdc50415372e9dd29c86c22e5daa3a
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-02-27 11:05:09 +0100

    comments and getters and setters for mouse click handler

--- src/mouse/MouseClickHandler.java
+++ src/mouse/MouseClickHandler.java
@@ -6,55 +6,109 @@ import visualobjects.Container;
 
 public class MouseClickHandler {
 	
-	private long lastClickTime;
-	private int lastClickX;
-	private int lastClickY;
-	private Container container;
-	private boolean isDown;
-	private boolean isBeingDragged;
-	
 	public MouseClickHandler(Container container){
-		this.lastClickTime = 0;
-		this.container = container;
-		this.isDown = false;
-		this.isBeingDragged = false;
-		this.lastClickX = -1;
-		this.lastClickY = -1;
+		setLastClickTime(0);
+		setContainer(container);
+		setDown(false);
+		setBeingDragged(false);
+		setLastClickX(-1);
+		setLastClickY(-1);
 	}
 	
 	public void handleKey(MouseEvent e){
 		if (e.getID() == MouseEvent.MOUSE_PRESSED){
 			this.isDown = true;
-			
-			if (System.currentTimeMillis() - this.lastClickTime < 200){
-				if (Math.abs(e.getX() - this.lastClickX) < 20 &&
-						Math.abs(e.getY() - this.lastClickY) < 20){
+			//Check if time until last click is lower than threshold
+			if (System.currentTimeMillis() - getLastClickTime() < 200){
+				//Check if click is in range of last click
+				if (Math.abs(e.getX() - getLastClickX()) < 20 &&
+						Math.abs(e.getY() - getLastClickY()) < 20){
 					//Doubleclick
-					container.select(e.getX(), e.getY(), MouseClick.DOUBLE_CLICK);
+					getContainer().select(e.getX(), e.getY(), MouseClick.DOUBLE_CLICK);
 				} else {
-					container.select(e.getX(), e.getY(), MouseClick.CLICK);
+					getContainer().select(e.getX(), e.getY(), MouseClick.CLICK);
 				}
 			} else {
-				container.select(e.getX(), e.getY(), MouseClick.CLICK);
+				getContainer().select(e.getX(), e.getY(), MouseClick.CLICK);
 			}
-			this.lastClickX = e.getX();
-			this.lastClickY = e.getY();
-			this.lastClickTime = System.currentTimeMillis();
-			
+			//save the data
+			setLastClickX(e.getX());
+			setLastClickY(e.getY());
+			setLastClickTime(System.currentTimeMillis());
 		}
 		
 		if (e.getID() == MouseEvent.MOUSE_RELEASED){
-			this.isDown = false;
-			if (this.isBeingDragged){
-				container.select(e.getX(), e.getY(), MouseClick.DRAG);
+			setDown(false);
+			if (isBeingDragged()){
+				getContainer().select(e.getX(), e.getY(), MouseClick.DRAG);
 			}
-			this.isBeingDragged = false;
+			setBeingDragged(false);
 		}
 		
 		if (e.getID() == MouseEvent.MOUSE_DRAGGED){
-			this.isBeingDragged = true;
+			setBeingDragged(true);
 		}
 	}
+	
+	private long getLastClickTime() {
+		return lastClickTime;
+	}
+
+	private void setLastClickTime(long lastClickTime) {
+		this.lastClickTime = lastClickTime;
+	}
+	
+	private long lastClickTime;
+
+	private int getLastClickX() {
+		return lastClickX;
+	}
+
+	private void setLastClickX(int lastClickX) {
+		this.lastClickX = lastClickX;
+	}
+	
+	private int lastClickX;
+
+	private int getLastClickY() {
+		return lastClickY;
+	}
+
+	private void setLastClickY(int lastClickY) {
+		this.lastClickY = lastClickY;
+	}
+	
+	private int lastClickY;
+
+	private Container getContainer() {
+		return container;
+	}
+
+	private void setContainer(Container container) {
+		this.container = container;
+	}
+	
+	private Container container;
+
+	private boolean isDown() {
+		return isDown;
+	}
+
+	private void setDown(boolean isDown) {
+		this.isDown = isDown;
+	}
+	
+	private boolean isDown;
+
+	private boolean isBeingDragged() {
+		return isBeingDragged;
+	}
+
+	private void setBeingDragged(boolean isBeingDragged) {
+		this.isBeingDragged = isBeingDragged;
+	}
+	
+	private boolean isBeingDragged;
 	
 	
 }
