972e01c89f1ac875f4722a42b04f0ad47242ee95
Stijn Caerts <stijncaerts@gmail.com>
2017-03-05 14:40:19 +0100

    documentatie in textBox

--- src/visualobjects/TextBox.java
+++ src/visualobjects/TextBox.java
@@ -6,6 +6,30 @@ import objects.Logical_objects;
 
 public class TextBox extends VisualObject {
 	
+	/**
+	 * @param 	x 
+	 * 			Coordinate on the x-axis of the top-left corner
+	 * @param 	y
+	 * 			Coordinate on the y-axis of the top-left corner
+	 * @param 	width
+	 * 			Width of the TextBox
+	 * @param 	height
+	 * 			Height of the TextBox
+	 * @param 	paddingLeft
+	 * 			Padding at the left side
+	 * @param 	paddingRight
+	 * 			Padding at the right side
+	 * @param 	paddingTop
+	 * 			Padding at the top
+	 * @param 	paddingBottom
+	 * 			Padding at the bottom
+	 * @param 	parent
+	 * 			VisualObject which is the parent of the object to be created
+	 * @param 	standardstring
+	 * 			String that is shown as text by default
+	 * @param 	object
+	 * 			Logical object of which this TextBox is the textual representation
+	 */
 	public TextBox(int x, int y, int width, int height, int paddingLeft, int paddingRight, 
 			int paddingTop, int paddingBottom, VisualObject parent, String standardstring, Logical_objects object){
 		super(x, y, width, height, parent);
@@ -19,6 +43,24 @@ public class TextBox extends VisualObject {
 		this.addChild(this.getText());
 	}
 	
+	/**
+	 * @param	x
+	 * 			Coordinate on the x-axis of the top-left corner
+	 * @param 	y
+	 * 			Coordinate on the y-axis of the top-left corner
+	 * @param 	width
+	 * 			Width of the TextBox
+	 * @param 	height
+	 * 			Height of the TextBox
+	 * @param 	padding
+	 * 			Size of padding at all sides
+	 * @param 	parent
+	 * 			VisualObject which is the parent of the object to be created
+	 * @param 	standardstring
+	 * 			String that is shown as text by default
+	 * @param 	object
+	 * 			Logical object of which this TextBox is the textual representation
+	 */
 	public TextBox(int x, int y, int width, int height, int padding, 
 			VisualObject parent, String standardstring, Logical_objects object){
 		this(x, y ,width, height, padding, padding, padding, padding, parent, standardstring, object);
