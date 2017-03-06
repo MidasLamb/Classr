ee9e93a101c8353a300530b5b829f8778fe56022
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-03-06 12:31:46 +0100

    alle muis testen volledig

---
+++ test/inputHandler/MouseClickHandlerTest.java
@@ -0,0 +1,290 @@
+package inputHandler;
+
+import static main.Constants.*;
+import static org.junit.Assert.*;
+
+import java.awt.Component;
+import java.awt.List;
+import java.awt.event.MouseEvent;
+
+import org.junit.Test;
+
+import inputHandlers.MouseClickHandler;
+import mouse.clicks.DoubleClick;
+import mouse.clicks.SingleClick;
+import visualobjects.Container;
+
+public class MouseClickHandlerTest {
+	
+	@Test
+	public void singleClickTest1(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		assertEquals(1, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void singleClickTest2(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void singleClickTest3(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		assertFalse(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest1(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		simulateSingleClick(handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTest2(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTest3(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest4(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE-1, 0, handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTest5(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE-1, 0, handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTest6(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE-1, 0, handler);
+		assertTrue(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest7(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE, 0, handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTest8(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE, 0, handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTest9(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(DOUBLECLICK_RANGE, 0, handler);
+		assertFalse(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest10(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE-1, handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTest11(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE-1, handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTest12(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE-1, handler);
+		assertTrue(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest13(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE, handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTest14(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE, handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTest15(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(0,0, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE, handler);
+		assertFalse(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest16(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(15,10, handler);
+		simulateSingleClick(0, DOUBLECLICK_RANGE, handler);
+		assertTrue(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTest17(){
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(8,15, handler);
+		simulateSingleClick(0, 15+DOUBLECLICK_RANGE, handler);
+		assertFalse(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTestTime1() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD-5);
+		simulateSingleClick(handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTestTime2() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD-5);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTestTime3() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD-5);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedDoubleClick);
+	}
+	
+	@Test
+	public void doubleClickTestTime4() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD+5);
+		simulateSingleClick(handler);
+		assertEquals(2, container.amountRecievedClicks);
+	}
+	
+	@Test
+	public void doubleClickTestTime5() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD+5);
+		simulateSingleClick(handler);
+		assertTrue(container.recievedSingleClick);
+	}
+	
+	@Test
+	public void doubleClickTestTime6() throws InterruptedException{
+		testContainer container = new testContainer();
+		MouseClickHandler handler = new MouseClickHandler(container);
+		simulateSingleClick(handler);
+		Thread.sleep(DOUBLECLICK_TRESHOLD+5);
+		simulateSingleClick(handler);
+		assertFalse(container.recievedDoubleClick);
+	}
+	
+	private static void simulateSingleClick(MouseClickHandler handler){
+		simulateSingleClick(0, 0, handler);
+	}
+	
+	private static void simulateSingleClick(int x, int y, MouseClickHandler handler){
+		Component dummy = new List();
+		MouseEvent event = new MouseEvent(dummy,MouseEvent.MOUSE_PRESSED,1,MouseEvent.BUTTON1,x,y,2,false);
+		handler.handleInput(event);
+	}
+	
+	class testContainer extends Container{
+
+		public testContainer() {
+			super(0,0,100,100);
+		}
+		
+		@Override
+		public void onClick(SingleClick sc){
+			amountRecievedClicks++;
+			recievedSingleClick = true;
+		}
+		
+		@Override
+		public void onDoubleClick(DoubleClick dc){
+			amountRecievedClicks++;
+			recievedDoubleClick = true;
+		}
+		
+		public int amountRecievedClicks;
+		public boolean recievedSingleClick;
+		public boolean recievedDoubleClick;
+		
+	}
+
+}
--- test/mouse/clicks/DoubleClickTest.java
+++ test/mouse/clicks/DoubleClickTest.java
@@ -14,55 +14,5 @@ import inputHandlers.MouseClickHandler;
 
 public class DoubleClickTest {
 	
-	@Test
-	public void doubleClicktest() throws InterruptedException{
-		assertTrue(doubleClickRegistrationTest(0, 0, DOUBLECLICK_TRESHOLD/2, 0, 0, 100, 100));
-
-	}
-	
-	@Test
-	public void slowDoubleClicktest() throws InterruptedException{
-		assertFalse(doubleClickRegistrationTest(0, 0, DOUBLECLICK_TRESHOLD*2, 0, 0, 100, 100));
-	}
-	
-	@Test
-	public void outOfContainerDoubleClicktest() throws InterruptedException{
-		assertFalse(doubleClickRegistrationTest(200, 0, DOUBLECLICK_TRESHOLD/2, 0, 0, 100, 100));
-	}
-
-	
-
-	/**
-	 * Tests the registration of a double click by simulating two different clicks
-	 * 
-	 * @param 	x
-	 * 			x-coordinate of the click
-	 * @param 	y
-	 * 			y-coordinate of the click
-	 * @param 	ms
-	 * 			amount of milliseconds between clicks
-	 * @param 	cX
-	 * 			x-coordinate of the container
-	 * @param 	cY
-	 * 			y-coordinate of the container
-	 * @param 	cW
-	 * 			width of the container
-	 * @param 	cH
-	 * 			Height of the container
-	 * @throws InterruptedException
-	 */
-	public boolean doubleClickRegistrationTest(int x, int y, int ms, int cX, int cY, int cW, int cH) throws InterruptedException {
-		Container container = new Container(cX,cY,cW,cH); //doesn't really matter
-		MouseClickHandler handler = new MouseClickHandler(container);
-		Component dummy = new List();
-		//first click
-		MouseEvent event = new MouseEvent(dummy,MouseEvent.MOUSE_PRESSED,1,MouseEvent.BUTTON1,x,y,2,false);
-		handler.handleInput(event);
-		//wait
-		Thread.sleep(ms);
-		//second click
-		event = new MouseEvent(dummy,MouseEvent.MOUSE_PRESSED,1,MouseEvent.BUTTON1,x,y,2,false);
-		return handler.isDoubleClick(event);
-	}
 
 }
