3263785a41058c5f4293ae4939411016b5428cbb
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-03-06 20:50:47 +0100

    tests

--- test/visualobjects/visualclass/VisualClassTest.java
+++ test/visualobjects/visualclass/VisualClassTest.java
@@ -11,59 +11,50 @@ public class VisualClassTest {
 	@Test
 	public void isInTest1() {
 		Container container = new Container(0, 0, 1000, 1000);
-		VisualClass visualClass = new VisualClass(0, 0, container);
+		VisualClass visualClass = new VisualClass(0, 0,0, container);
 		assertTrue(visualClass.isIn(0, 0));
 	}
 	
 	@Test
 	public void isInTest2() {
 		Container container = new Container(0, 0, 1000, 1000);
-		VisualClass visualClass = new VisualClass(0, 0, container);
+		VisualClass visualClass = new VisualClass(0, 0, 0, container);
 		assertFalse(visualClass.isIn(101, 0));
 	}
 	
 	@Test
 	public void isInTest3() {
 		Container container = new Container(0, 0, 1000, 1000);
-		VisualClass visualClass = new VisualClass(10, 5, container);
+		VisualClass visualClass = new VisualClass(10, 5, 0, container);
 		assertTrue(visualClass.isIn(10, 5));
 	}
 	
 	@Test
 	public void isInTest4() {
 		Container container = new Container(0, 0, 1000, 1000);
-		VisualClass visualClass = new VisualClass(10, 5, container);
+		VisualClass visualClass = new VisualClass(10, 5, 0, container);
 		assertTrue(visualClass.isIn(visualClass.getWidth()+10, 5));
 	}
 	
 	@Test
 	public void isInTest5() {
 		Container container = new Container(0, 0, 1000, 1000);
-		VisualClass visualClass = new VisualClass(10, 5, container);
+		VisualClass visualClass = new VisualClass(10, 5, 0, container);
 		assertTrue(visualClass.isIn(10, visualClass.getHeight()+4));
 	}
 	
 	@Test
 	public void isInTest6() {
 		Container container = new Container(10, 5, 100, 100);
-		VisualClass visualClass = new VisualClass(10, 5, container);
+		VisualClass visualClass = new VisualClass(10, 5, 0, container);
 		assertFalse(visualClass.isIn(visualClass.getWidth()+11, 5));
 	}
 	
 	@Test
 	public void isInTest7() {
 		Container container = new Container(10, 5, 100, 100);
-		VisualClass visualClass = new VisualClass(0, 0, container);
+		VisualClass visualClass = new VisualClass(0, 0, 0, container);
 		assertFalse(visualClass.isIn(10, visualClass.getHeight()+6));
-	}
-	
-	@Test
-	public void deleteTest() {
-		Container container = new Container(10, 5, 100, 100);
-		VisualClass visualClass = new VisualClass(0, 0, container);
-		visualClass.delete();
-		Graphics g = new Grap
-		
 	}
 
 }
