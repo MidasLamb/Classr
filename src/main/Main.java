84c266e76a80e20dd928ee5dddb789e14332d2d8
Stijn Caerts <stijncaerts@gmail.com>
2017-03-20 12:18:07 +0100

    main formatting

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		java.awt.EventQueue.invokeLater(() -> {
-			          new MyCanvasWindow(CANVAS_TITLE).show();
-			      });
+			new MyCanvasWindow(CANVAS_TITLE).show();
+		});
 	}
 
 }
