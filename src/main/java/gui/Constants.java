36f224326bc21a68f71af9d5087b26eff4819963
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-03-29 10:32:23 +0200

    label

--- src/main/java/gui/Constants.java
+++ src/main/java/gui/Constants.java
@@ -4,5 +4,6 @@ public final class Constants {
 	
 	public static final int STANDARD_RADIOBUTTON_WIDTH = 20;
 	public static final int STANDARD_RADIOBUTTON_HEIGHT = 20;
+	public static final int STANDARD_TEXT_HEIGHT = 16;
 
 }
