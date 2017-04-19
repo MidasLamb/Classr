b1d67f0216c4595f299086ede461da41991a8794
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-04-19 11:09:43 +0200

    TextWrapper fixed

--- src/main/java/visualobjects/EditableTextWrapper.java
+++ src/main/java/visualobjects/EditableTextWrapper.java
@@ -24,93 +24,90 @@ public class EditableTextWrapper extends TextWrapper {
 	private String standardString;
 	private String regex;
 
-	public EditableTextWrapper(int x, int y, int z, String string, String regex, VisualObject parent, LogicalObject object) {
-		super(x,y,z, parent, object);
+	public EditableTextWrapper(int x, int y, int z, String string, String regex, VisualObject parent,
+			LogicalObject object) {
+		super(x, y, z, parent, object);
 		setLogicalObject(object);
 		this.setTextObject(new Text(new AttributedString(""), new PassiveState()));
 		this.setStandardString(string);
 		this.setRegex(regex);
 	}
-	
-	public EditableTextWrapper(int x, int y, int z, String string,VisualObject parent, LogicalObject object) {
-		this(x,y,z,string, ".*", parent, object);
+
+	public EditableTextWrapper(int x, int y, int z, String string, VisualObject parent, LogicalObject object) {
+		this(x, y, z, string, ".*", parent, object);
 	}
 
 	/**
 	 * Shows the text frame
 	 */
 	@Override
-	public void draw(Graphics g) {	
+	public void draw(Graphics g) {
 		this.getTextObject().draw(g, this.getX(), this.getY());
 	}
-	
+
 	/**
 	 * @return Returns the text of the Logical Object
 	 */
-	protected AttributedString getText(){
+	protected AttributedString getText() {
 		StringVisitor strVis = new StringVisitor();
 		return strVis.startVisit(this.getLogicalObject());
 	}
-	
-	public final String getCurrentDisplayedString(){
+
+	public final String getCurrentDisplayedString() {
 		return this.getTextObject().getText();
 	}
-	
+
 	@Override
-	void onClick(SingleClick sc){
+	void onClick(SingleClick sc) {
 		this.getContainer().switchSelectedTo(this);
 	}
 
-	
 	@Override
-	public void setSelected(boolean b){
+	public void setSelected(boolean b) {
 		super.setSelected(b);
 		if (b) {
 			this.getTextObject().switchState(new EditableState());
 		} else {
 			this.getTextObject().switchState(new PassiveState());
-			if (this.getTextObject().getText().length() == 0){
-				this.getLogicalObject().setName(this.getStandardString());
-				this.getTextObject().setAttributedText(getText());
-			}
+			this.save();
 		}
-		
+
 		this.getTextObject().setAttributedText(getText());
 	}
 
 	@Override
-	void onDoubleClick(DoubleClick sc){
+	void onDoubleClick(DoubleClick sc) {
 		this.getContainer().switchSelectedTo(this);
 	}
-	
+
 	@Override
-	int getWidth(){
+	int getWidth() {
 		return STANDARD_FONTMETRICS.stringWidth(getString());
 	}
-	
+
 	@Override
-	int getHeight(){
+	int getHeight() {
 		return STANDARD_TEXT_HEIGHT;
 	}
 
 	@Override
-	public void handleAsciiKey(AsciiKey key){
+	public void handleAsciiKey(AsciiKey key) {
 		this.getTextObject().handleAsciiKey(key);
 	}
-	
+
 	@Override
-	public void handleFunctionKey(FunctionKey key){
-		
-		switch(key.getKeyType()){
+	public void handleFunctionKey(FunctionKey key) {
+
+		switch (key.getKeyType()) {
 		case ENTER:
-			if (this.satisfiesRegex()){
+			if (this.satisfiesRegex()) {
 				this.quitAndSave();
 				this.getTextObject().handleFunctionKey(key);
-				}
+			}
 			break;
 		case ESCAPE:
 			this.getTextObject().handleFunctionKey(key);
-			this.quitAndDiscard();
+			this.quit();
 			break;
 		case DELETE:
 			this.getTextObject().handleFunctionKey(key);
@@ -130,13 +127,24 @@ public class EditableTextWrapper extends TextWrapper {
 		this.standardString = standardString;
 	}
 	
-	private void quitAndSave(){
-		this.getLogicalObject().setName(this.getTextObject().getText());
-		this.getContainer().switchSelectedTo(null);
+	/**
+	 * Saves the currently displayed text to the logical object if the Regex is satisfied and length is > 0.
+	 */
+	private void save(){
+		if (this.getCurrentDisplayedString().length() == 0 || !this.satisfiesRegex()) {
+			this.getLogicalObject().setName(this.getStandardString());
+			this.getTextObject().setAttributedText(getText());
+		}
+		
 	}
 	
-	private void quitAndDiscard(){
+	private void quit() {
 		this.getContainer().switchSelectedTo(null);
+	}
+
+	private void quitAndSave() {
+		this.save();
+		this.quit();
 	}
 
 	private final String getRegex() {
@@ -146,8 +154,8 @@ public class EditableTextWrapper extends TextWrapper {
 	private final void setRegex(String regex) {
 		this.regex = regex;
 	}
-	
-	private final boolean satisfiesRegex(){
+
+	private final boolean satisfiesRegex() {
 		return this.getCurrentDisplayedString().matches(this.getRegex());
 	}
 }
