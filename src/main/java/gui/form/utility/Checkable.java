f9586622cdd084e82274a4d644d2c512a27ccc7a
Stijn Caerts <stijncaerts@gmail.com>
2017-04-20 14:39:37 +0200

    Checker and Checkable docs

--- src/main/java/gui/form/utility/Checkable.java
+++ src/main/java/gui/form/utility/Checkable.java
@@ -1,6 +1,19 @@
 package gui.form.utility;
 
+/**
+ * Constraint that needs to be checked.
+ */
 public interface Checkable {
+	/**
+	 * @return true if the constraint is fulfilled, false otherwise
+	 */
 	public boolean check();
+
+	/**
+	 * Add a Checker to this Checkable
+	 * 
+	 * @param c
+	 *            Checker to be added
+	 */
 	void addChecker(Checker c);
 }
