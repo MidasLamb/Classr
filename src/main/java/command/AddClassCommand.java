2b014d9928de96e6ab90afc8b341f9484e8fea4b
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-05-08 12:12:52 +0200

    controller added

---
+++ src/main/java/command/AddClassCommand.java
@@ -0,0 +1,21 @@
+package command;
+
+public class AddClassCommand extends Command {
+
+	public AddClassCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void unexecute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/command/Command.java
@@ -0,0 +1,9 @@
+package command;
+
+abstract class Command {
+
+	 abstract void execute();
+	 
+	 abstract void unexecute();
+
+}
