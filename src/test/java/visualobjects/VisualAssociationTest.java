49ac97006a88e74e7f465ca90dea21cb646d8d79
Stijn Caerts <stijncaerts@gmail.com>
2017-05-08 13:24:02 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/test/java/visualobjects/VisualAssociationTest.java
@@ -0,0 +1,37 @@
+package visualobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gui.inputHandlers.clicks.DoubleClick;
+import gui.inputHandlers.clicks.Drag;
+import gui.inputHandlers.clicks.SingleClick;
+import gui.inputHandlers.keys.FunctionKey;
+import gui.inputHandlers.keys.FunctionKey.FunctionKeyType;
+
+public class VisualAssociationTest {
+
+	@Test
+	public void deleteOnDeleteClassTest() {
+		Container container = new Container(0, 0, 1000, 1000);
+		// Create new class
+		DoubleClick click1 = new DoubleClick(103,108);
+		// Create new association
+		Drag drag = new Drag(104,142,104,142);
+		// Deselect all objects
+		SingleClick click2 = new SingleClick(1,1);
+		
+		container.onDoubleClick(click1);
+		container.onDragEnd(drag);
+		container.onClick(click2);
+		
+		// Select class
+		container.onClick(new SingleClick(103, 108));
+		
+		container.handleFunctionKey(new FunctionKey(FunctionKeyType.DELETE));
+		
+		assertEquals(0, container.getChildren().size());
+	}
+
+}
