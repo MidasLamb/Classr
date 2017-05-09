1550f4af78342be785690122a0cc55c0df3d1ffe
Pieter-Jan <pieterjan.coenen@gmail.com>
2017-05-09 17:07:40 +0200

    commentaar attribute

--- src/main/java/command/CreateAttributeCommand.java
+++ src/main/java/command/CreateAttributeCommand.java
@@ -4,11 +4,19 @@ import visualobjects.PaddingBox;
 import visualobjects.TextWrapper;
 import visualobjects.VisualClass;
 
+/**
+ * A command for creating an attribute in a class
+ */
 public class CreateAttributeCommand extends Command {
 
 	private final VisualClass visualClass;
 	private PaddingBox<TextWrapper> attributePaddingBox;
 	
+	/**
+	 * The constructor
+	 * @param 	visualClass
+	 * 			the visual class in which the attribute needs to be created
+	 */
 	public CreateAttributeCommand(VisualClass visualClass) {
 		this.visualClass = visualClass;
 	}
@@ -23,14 +31,27 @@ public class CreateAttributeCommand extends Command {
 		this.getAttributePaddingBox().delete();
 	}
 
+	/**
+	 * Returns the padding box in which the created attribute is
+	 * @return the padding box in which the created attribute is
+	 */
 	private final PaddingBox<TextWrapper> getAttributePaddingBox() {
 		return attributePaddingBox;
 	}
 
+	/**
+	 * Sets the padding box in which the created attribute is
+	 * @param 	attributePaddingBox
+	 * 			the padding box in which the created attribute is
+	 */
 	private final void setAttributePaddingBox(PaddingBox<TextWrapper> attributePaddingBox) {
 		this.attributePaddingBox = attributePaddingBox;
 	}
 
+	/**
+	 * Returns the visual class in which the attribute is
+	 * @return the visual class in which the attribute
+	 */
 	private final VisualClass getVisualClass() {
 		return visualClass;
 	}
