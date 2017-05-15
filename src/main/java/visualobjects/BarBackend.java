0d59af079a3f6f69e9bc243e1eb20b1a5ee0d277
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-05-15 13:35:21 +0200

    Worked some more on backend

--- src/main/java/visualobjects/BarBackend.java
+++ src/main/java/visualobjects/BarBackend.java
@@ -1,9 +1,15 @@
 package visualobjects;
 
+import command.Command;
 import command.Controller;
+import command.CreateAttributeCommand;
+import command.CreateClassCommand;
+import command.CreateMethodCommand;
+import logicalobjects.Attribute;
+import logicalobjects.Method;
 
 class BarBackend {
-	
+
 	private static Controller controller;
 	private static Container container;
 
@@ -11,8 +17,7 @@ class BarBackend {
 		BarBackend.setContainer(container);
 		BarBackend.setController(controller);
 	}
-	
-	
+
 	private static final Controller getController() {
 		return controller;
 	}
@@ -30,69 +35,77 @@ class BarBackend {
 		if (BarBackend.container == null)
 			BarBackend.container = container;
 	}
-	
-	public static final void createClass(){
-		//TODO
+
+	public static final void createClass() {
+		getController().executeCommand(new CreateClassCommand(getContainer()));
 	}
-	
-	public static final void addAttribute(){
-		//TODO
+
+	public static final void addAttribute() {
+		if (canAddAttribute()){	
+			Command c = new CreateAttributeCommand(((VisualClass) getContainer().getSelected()));
+			getController().executeCommand(c);
+		}
+			
 	}
-	
-	public static final void addMethod(){
-		//TODO
+
+	public static final void addMethod() {
+		if (canAddMethod()){	
+			Command c = new CreateMethodCommand(((VisualClass) getContainer().getSelected()));
+			getController().executeCommand(c);
+		}
 	}
-	
-	public static final void addParameter(){
-		//TODO
+
+	public static final void addParameter() {
+		// TODO
 	}
-	
-	public static final void editName(){
-		//TODO
+
+	public static final void editName() {
+		// TODO
 	}
-	
-	public static final void editTripleDot(){
-		//TODO
+
+	public static final void editTripleDot() {
+		// TODO
 	}
-	
-	public static final void delete(){
-		//TODO
+
+	public static final void delete() {
+		if (canDelete()){
+			getContainer().getSelected().delete();
+		}
 	}
-	
-	public static final void undo(){
+
+	public static final void undo() {
 		getController().undo();
 	}
-	
-	public static final void redo(){
+
+	public static final void redo() {
 		getController().redo();
 	}
-	
-	public static final boolean canAddAttribute(){
-		//TODO
+
+	public static final boolean canAddAttribute() {
+		return getContainer().getSelected() instanceof VisualClass;
+	}
+
+	public static final boolean canAddMethod() {
+		return getContainer().getSelected() instanceof VisualClass;
+	}
+
+	public static final boolean canAddParameter() {
+		return getContainer().getSelected().getLogicalObject() instanceof Method;
+	}
+
+	public static final boolean canEditName() {
+		return getContainer().getSelected().getLogicalObject() instanceof Method
+				|| getContainer().getSelected().getLogicalObject() instanceof Attribute;
+	}
+
+	public static final boolean canEditTripleDot() {
+		// TODO
 		return false;
 	}
-	
-	public static final boolean canAddMethod(){
-		//TODO
+
+	public static final boolean canDelete() {
+		// TODO
 		return false;
 	}
-	public static final boolean canAddParameter(){
-		//TODO
-		return false;
-	}
-	public static final boolean canEditName(){
-		//TODO
-		return false;
-	}
-	public static final boolean canEditTripleDot(){
-		//TODO
-		return false;
-	}
-	public static final boolean canDelete(){
-		//TODO
-		return false;
-	}
-	
-	
 
 }
