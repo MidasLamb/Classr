23eb341f26d6495ec79b84cf250b4f5aaeb07352
Dries De Proost <driesdeproost@hotmail.com>
2017-05-22 13:41:56 +0200

    docs updatelistener updatesubject

--- src/main/java/interfaces/UpdateSubject.java
+++ src/main/java/interfaces/UpdateSubject.java
@@ -1,7 +1,25 @@
 package interfaces;
 
+/**
+ * An interface for classes that want to have updateListeners
+ */
 public interface UpdateSubject {
+	/**
+	 * To add a new updateListener
+	 * @param 	updateListener
+	 * 			the new updateListener
+	 */
 	public void addUpdateListener(UpdateListener updateListener);
+	
+	/**
+	 * To remove a updateListener
+	 * @param 	updateListener
+	 * 			the updateListener that need to be removed
+	 */
 	public void removeUpdateListener(UpdateListener updateListener);
+	
+	/**
+	 * A function that notifies the updateListeners
+	 */
 	public void notifyUpdateListeners();
 }
