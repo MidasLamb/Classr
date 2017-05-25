dd9852b607459c677eac8b3447593d5423ae9072
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-05-25 13:04:03 +0200

    move and resize tests

--- src/test/java/canvaswindow/VisualTests.java
+++ src/test/java/canvaswindow/VisualTests.java
@@ -3,23 +3,32 @@ package canvaswindow;
 import static main.Constants.CANVAS_TITLE;
 import static org.junit.Assert.assertTrue;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.OptionalInt;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
-
-import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
 public class VisualTests {
+	
+	@Test
+	public void moveClassAndContentBoxTest() throws IOException {
+		String name = "moveClassAndContentBoxTest";
+		MyCanvasWindow canvasWindow = new MyCanvasWindow(CANVAS_TITLE);
+		assertTrue(MyCanvasWindow.replayRecording("recordings/"+name+"/recording", canvasWindow));
+	}
+	
+	@Test
+	public void resizeContentBoxTest() throws IOException {
+		String name = "resizeContentBoxTest";
+		MyCanvasWindow canvasWindow = new MyCanvasWindow(CANVAS_TITLE);
+		assertTrue(MyCanvasWindow.replayRecording("recordings/"+name+"/recording", canvasWindow));
+	}
+	
+	@Test
+	public void resizeClassTest() throws IOException {
+		String name = "resizeClassTest";
+		MyCanvasWindow canvasWindow = new MyCanvasWindow(CANVAS_TITLE);
+		assertTrue(MyCanvasWindow.replayRecording("recordings/"+name+"/recording", canvasWindow));
+	}
 
 	@Test
 	public void barTest() throws IOException {
