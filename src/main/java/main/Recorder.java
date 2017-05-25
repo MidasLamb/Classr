6f08a35ddeb447b71dcc6c060cece9181c9098b0
Midas Lambrichts <midas_lambrichts@hotmail.com>
2017-05-25 14:47:33 +0200

    MORE TEZTZ

--- src/main/java/main/Recorder.java
+++ src/main/java/main/Recorder.java
@@ -13,7 +13,7 @@ import canvaswindow.MyCanvasWindow;
 public class Recorder {
 
 	public static void main(String[] args) {
-		String testName = "createAttributeTest";
+		String testName = "autoresizeTest";
 		prepareDirectory(testName);
 		MyCanvasWindow canvasWindow = new MyCanvasWindow(CANVAS_TITLE);
 		canvasWindow.recordSession("recordings/" + testName + "/recording");
